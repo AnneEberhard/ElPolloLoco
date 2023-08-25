204cf8d30edf412bef2494743288fbb10c478960
AnneEberhard <anne.eberhard@gmx.net>
2023-08-25 18:34:42 +0200

    chicken on random start

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,5 +2,6 @@ class Chicken extends MovableObject {
   constructor() {
     // wird immer aufgerufen, wenn die Klasse aufgerufen wird
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png"); //greift auf übergeordnete Klasse zu
+    this.x = 200+ Math.random()*500;
   }
 }
