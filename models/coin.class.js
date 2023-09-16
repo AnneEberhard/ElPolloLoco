4011e530329faf36d14f5a08f5d08dfc851929f8
AnneEberhard <anne.eberhard@gmx.net>
2023-09-16 08:23:46 +0200

    coin animation added

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,33 +1,29 @@
 class Coin extends MovableObject {
-    height = 200;
-    width = 200;
-  
-    IMAGES_COIN = [
-      "img/8_coin/coin_1.png",
-      "img/8_coin/coin_2.png"
-     ]
+  height = 200;
+  width = 200;
 
-    offset = {
-        top: 70,
-        right:70,
-        bottom:70,
-        left:70
-      }
+  IMAGES_COIN = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
 
-    constructor() {
-        super().loadImage("img/8_coin/coin_2.png"); 
-        this.loadImages(this.IMAGES_COIN);
-        this.x = 100 + Math.random() * 2000;
-        this.y = Math.random() * 300;
-        this.animate();
-    }
+  offset = {
+    top: 70,
+    right: 70,
+    bottom: 70,
+    left: 70,
+  };
 
-    animate() {
-      setStoppableInterval(this.coinImages.bind(this), 100);
-    }
-
-    coinImages() {
-      if (this.gameIsRunning()) {
-          this.playAnimation(this.IMAGES_COIN);
-    } }
+  constructor() {
+    super().loadImage("img/8_coin/coin_2.png");
+    this.loadImages(this.IMAGES_COIN);
+    this.x = 100 + Math.random() * 2000;
+    this.y = Math.random() * 300;
+    this.animate();
   }
+
+  animate() {
+    setStoppableInterval(this.coinImages.bind(this), 200);
+  }
+
+  coinImages() {
+    this.playAnimation(this.IMAGES_COIN);
+  }
+}
